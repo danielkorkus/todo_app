76b08a25b5ecc679a742b4918695676b534209a2
danielkorkus <daniel.korkus94@gamil.com>
2023-03-22 18:31:19 +0100

    added handleupdate/change/delete functions

--- frontend/src/components/TodoForm.js
+++ frontend/src/components/TodoForm.js
@@ -3,21 +3,45 @@ import Button from "react-bootstrap/Button";
 import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import Form from "react-bootstrap/Form";
+import axios from "axios";
 
+export default function TodoForm({ todos, setTodos }) {
+    const [name, setName] = useState("");
 
-export default function Todo_Form() {
-        const [name, setName] = useState("");
-        const handleChange = e => {
-            setName(e.target.value);
+    const handleChange = e => {
+        setName(e.target.value);
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!name) {
+            alert("Please provide a valid value for todo");
+            return;
         }
 
-    return <Form>
-            <InputGroup className="mb-4">
-                <FormControl placeholder="New Todo" onChange={handleChange} value={name}/>
-                <Button type="submit">
-                    Add
-                </Button>
-            </InputGroup>
-        </Form>
+        axios.post("/api/todos/", {
+            name: name
+        }).then((res) => {
+            setName("");
+            const { data } = res;
+            setTodos([
+                ...todos,
+                data
+            ]).catch(() => {
+                alert("Something went wrong");
+            })
+        })
+    }
 
+    return <Form onSubmit={handleSubmit}>
+        <InputGroup className="mb-4">
+            <FormControl placeholder="New Todo"
+                onChange={handleChange}
+                value={name}
+            />
+            <Button type="submit">
+                Add
+            </Button>
+        </InputGroup>
+    </Form>
 }
